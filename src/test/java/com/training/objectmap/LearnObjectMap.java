7e94b10453347ce406cb32eaac2b604f84e2fde6
veerabhaskar <veeruveeru4024@gmail.com>
2020-12-29 17:43:55 +0530

    dvsdv

--- src/test/java/com/training/objectmap/LearnObjectMap.java
+++ src/test/java/com/training/objectmap/LearnObjectMap.java
@@ -9,7 +9,7 @@ public interface LearnObjectMap {
 	String FB_LOGIN_ERROR_MSG_XPATH="//div[@id='loginform']/div[2]/div[2]";
 	  
 	String FB_REGISTER_CREATENEWACCOUNT_XPATH = "//a[@role='button']";
-	String FB_REGISTER_FIRSTNAME_XPATH = "//input[@placeholder='First name']";
+	String FB_REGISTER_FIRSTNAME_XPATH = "//input[@placeholder='First name']"; 
 	String FB_REGISTER_LASTNAME_XPATH = "//input[@name='lastname']";   
 	
 	 
